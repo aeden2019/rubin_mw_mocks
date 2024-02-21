08e1e5f88adb8794225eb380f071e1e97842218d
Default user <aeden2019@my.fit.edu>
2024-02-21 19:47:31 +0000

    Renamed make_mock_anankethon.py to make_mock.py and removed make_mock.py

--- src/mock/make_mock.py
+++ src/mock/make_mock.py
@@ -1,5 +1,6 @@
 """
-Create ANANKE mock catalog from FIRE data
+Create ANANKE mock catalog from FIRE data with the intent of replicating the anankethon results
+(see https://github.com/athob/anankethon/blob/main/usage_example.ipynb)
 
 """
 
@@ -17,30 +18,47 @@ if __name__ == "__main__":
  
     # Get the parameters from the config file
     params = allvariables.getparams()
+
+    # Define parameter names
+    param_names = [
+        "snap", "sim", "sim_dir", "sat_id_dir", "rmin", "rmax", 
+        "bmin", "bmax", "overdensity", "sat_mask", "only_sat_mask", 
+        "halo_mask", "rand_mask", "subsample", "ananke_name", 
+        "photo_sys", "cmd_magnames", "app_mag_lim_lo", "app_mag_lim_hi", 
+        "abs_mag_lim_lo", "abs_mag_lim_hi", "ananke_r_max", "fsample"
+    ]
+
+    # Initialize parameter dictionary
+    params_dict = {}
+
+    # Assign values to parameters and declare them locally
+    for param_name, param_value in zip(param_names, params):
+        params_dict[param_name] = param_value
+        locals()[param_name] = param_value
     
-    # Declare all the parameters locally
-    snap = params[0]
-    sim = params[1]
-    sim_dir = params[2]
-    sat_id_dir = params[3]
-    rmin = params[4]
-    rmax = params[5]
-    bmin = params[6]
-    bmax = params[7]
-    overdensity = params[8]
-    sat_mask = params[9]
-    only_sat_mask = params[10]
-    halo_mask = params[11]
-    rand_mask = params[12]
-    subsample = params[13]
-    ananke_name = params[14]
-    photo_sys = params[15]
-    cmd_magnames = params[16]
-    app_mag_lim_lo = params[17]
-    app_mag_lim_hi = params[18]
-    abs_mag_lim_lo = params[19]
-    abs_mag_lim_hi = params[20]
-    ananke_r_max = params[21]
+#     # Declare all the parameters locally
+#     snap = params[0]
+#     sim = params[1]
+#     sim_dir = params[2]
+#     sat_id_dir = params[3]
+#     rmin = params[4]
+#     rmax = params[5]
+#     bmin = params[6]
+#     bmax = params[7]
+#     overdensity = params[8]
+#     sat_mask = params[9]
+#     only_sat_mask = params[10]
+#     halo_mask = params[11]
+#     rand_mask = params[12]
+#     subsample = params[13]
+#     ananke_name = params[14]
+#     photo_sys = params[15]
+#     cmd_magnames = params[16]
+#     app_mag_lim_lo = params[17]
+#     app_mag_lim_hi = params[18]
+#     abs_mag_lim_lo = params[19]
+#     abs_mag_lim_hi = params[20]
+#     ananke_r_max = params[21]
     
     # Print parameters (for debugging)
     print(f"Parameters:")
@@ -66,18 +84,58 @@ if __name__ == "__main__":
     print(f"  abs_mag_lim_lo: {abs_mag_lim_lo}")
     print(f"  abs_mag_lim_hi: {abs_mag_lim_hi}")
     print(f"  ananke_r_max: {ananke_r_max}")
+    print(f"  fsample: {fsample}")
+    
+    
+    
      
+    # Read in star particles from the selected fire2 snapshot
+    #redshift = 0.50239229 #snap 385
+    redshift = 0.
     
-    # Create FIRE part
-    part = ga.io.Read.read_snapshots(['star'], 'index', snap, sim_dir, assign_hosts=True)
+    part = ga.io.Read.read_snapshots(species='star',
+                                 snapshot_value_kind='redshift',
+                                 snapshot_values=redshift,
+                                 simulation_directory=sim_dir,
+                                 elements='all',
+                                 assign_hosts=True,
+                                 assign_hosts_rotation=True,
+                                 assign_orbits=True)
     
-    # Declare position and velocity
-    pos = part['star'].prop('host.distance')
-    vel = part['star'].prop('host.velocity')
     
+    
+    
+    # array of positions in principal axes (in kpc)
+    pos_pa = part['star'].prop('host.distance.principal')
+
+    # array of velocities in principal axes (in km/s)
+    vel_pa = part['star'].prop('host.velocity.principal')
+
+    # array of star particle mass in solar masses
+    mass = part['star']['mass']
+
+    # array of decimal log stellar ages (in Gyr)
+    log_age = np.log10(part['star'].prop('age') * 1e9)
+
+    # array of star particle metallicities
+    feh = part['star'].prop('metallicity.fe')
+
+    abundances_list = ['helium', 'carbon', 'nitrogen', 'oxygen', 'neon', 'magnesium', 'silicon', 'sulfur', 'calcium']
+    # dictionary of chemical abundance arrays (X/H)
+    abundances = {'sulphur' if el == 'sulfur' else el: part['star'].prop('metallicity.' + el) for el in abundances_list}
+
+    # alpha abundance (Mg/Fe)
+    alpha = abundances['magnesium'] - feh
+    
+    
+    
+    
+    # Preparing the star particles data to be used by ananke, masking only a sphere within 30 kpc
+    #mask = np.linalg.norm(pos_pa, axis=1)<=30
+    distance_mask = (np.linalg.norm(pos_pa, axis=1) >= rmin) & (np.linalg.norm(pos_pa, axis=1) <= rmax)
     
     # Blank satellite mask
-    satellite_mask = np.ones(len(pos), dtype=bool)
+    satellite_mask = np.ones(len(pos_pa), dtype=bool)
     
     # Store satellite indices (from Mia)
     all_unique_lmc_inds = np.loadtxt(sat_id_dir)
@@ -91,87 +149,35 @@ if __name__ == "__main__":
         print("* Removing satellite with mask \n")
         
         # Update satellite mask 
-        satellite_mask[only_lmc_indices] = 0
+        satellite_mask[only_lmc_indices] = False
+        
+   # Combine masks
+    mask = distance_mask & satellite_mask
+        
+              
 
-        # Apply satellite mask
-        pos = pos[satellite_mask]     
-        vel = vel[satellite_mask]
-        
-    # Include only the satellite mask
-    if only_sat_mask:
-        print("* Removing non-satellite objects with mask \n")
-        
-        # Update satellite mask 
-        satellite_mask[id_indices] = 0
-
-        # Apply satellite mask
-        pos = pos[satellite_mask]     
-        vel = vel[satellite_mask]
-         
-            
-    # Blank halo mask
-    dist_cut1 = np.ones(len(pos), dtype=bool)
-        
-    # Removing outer halo
-    if halo_mask:
-        print("* Removing halo with mask \n")
-        # Create outer halo distance mask
-        dist = np.sqrt(np.sum(pos**2, axis=1))
-        dist_cut1 = np.where((dist > rmin) & (dist< rmax)) 
-    
-        # Apply outer halo distance mask
-        pos = pos[dist_cut1]
-        vel = vel[dist_cut1]
-        
-    
-    # Blank random mask   
-    rand_cut1 = np.ones(len(pos), dtype=bool)
-   
-    # Apply random subsampling
-    if rand_mask:
-        print(f"* Applying subsample of {subsample} particles to the dataset of {len(pos)} particles \n")
-        # Create random cut
-        rand_cut1 = np.random.randint(0, len(pos), subsample)
-        pos = pos[rand_cut1]
-        vel = vel[rand_cut1]
-     
-    #Create p dictionary to store particle data
     p = {}
-    p['pos3'] = pos       # position in kpc
-    p['vel3'] = vel       # velocity in km/s
-    p['mass'] = part['star']['mass'][satellite_mask][dist_cut1][rand_cut1]                     # mass in solar masses
-    p['age'] = part['star'].prop('age')[satellite_mask][dist_cut1][rand_cut1]                  # log age in Gyr
-    p['feh'] = part['star'].prop('metallicity.fe')[satellite_mask][dist_cut1][rand_cut1]       # [Fe/H]
-    p['helium'] = part['star'].prop('metallicity.he')[satellite_mask][dist_cut1][rand_cut1]    # [He/H]
-    p['carbon'] = part['star'].prop('metallicity.c')[satellite_mask][dist_cut1][rand_cut1]     # [C/H]
-    p['nitrogen'] = part['star'].prop('metallicity.n')[satellite_mask][dist_cut1][rand_cut1]   # [N/H]
-    p['neon'] = part['star'].prop('metallicity.ne')[satellite_mask][dist_cut1][rand_cut1]      # [Ne/H]
-    p['magnesium'] = part['star'].prop('metallicity.mg')[satellite_mask][dist_cut1][rand_cut1] # [Mg/H]
-    p['silicon'] = part['star'].prop('metallicity.si')[satellite_mask][dist_cut1][rand_cut1]   # [Si/H]
-    p['sulphur'] = part['star'].prop('metallicity.s')[satellite_mask][dist_cut1][rand_cut1]    # [S/H]
-    p['calcium'] = part['star'].prop('metallicity.ca')[satellite_mask][dist_cut1][rand_cut1]   # [Ca/H]
-    p['oxygen'] = part['star'].prop('metallicity.o')[satellite_mask][dist_cut1][rand_cut1]     # [O/H]
-    p['alpha'] = part['star'].prop('metallicity.mg - metallicity.fe')[satellite_mask][dist_cut1][rand_cut1] # [Mg/Fe]
-    p['parentid'] = part['star']['id'][satellite_mask][dist_cut1][rand_cut1] # indices of parent particles in snapshot
-    p['dform'] = np.zeros(part['star']['position'].shape[0], dtype='float32')[satellite_mask][dist_cut1][rand_cut1] # dummy var
-   
-    # Assert that the lengths are the same
-    assert len(p['pos3']) == len(p['vel3']), "Pos has different length than vel when creating p dictionary"
-    assert len(p['pos3']) == len(p['mass']), "Pos has different length than mass when creating p dictionary"
+    p['pos3'] = pos_pa[mask]    # position in kpc (Nx3)
+    p['vel3'] = vel_pa[mask]    # velocity in km/s (Nx3)
+    p['mass'] = mass[mask]      # mass in solar masses
+    p['age'] = log_age[mask]    # log age in Gyr
+    p['feh'] = feh[mask]        # [Fe/H]
+    for el, abun in abundances.items():  
+        p[el] = abun[mask]      # other abundances as [X/H]
+
+    p['alpha'] = alpha[mask]    # alpha abundance [Mg/Fe]
+
+    p['parentid'] = np.where(mask)[0]           # indices of parent particles in snapshot
+    p['dform'] = 0*p['mass']  # dummy variable for now
     
-    # Plotting for testing
-    print("Plotting stellar ages and metalicity of ananke input")
-    plot_ananke_inputs.plot_stellar_ages(p['age'])
-    plot_ananke_inputs.plot_metalicity(p['feh'])
     
-    fsample = 0.05
     
-    # Initialize the ananke process with kword args
-    ananke = an.Ananke(p, fsample=fsample , name=ananke_name, photo_sys=photo_sys, cmd_magnames=cmd_magnames, 
-                       app_mag_lim_lo=app_mag_lim_lo, app_mag_lim_hi=app_mag_lim_hi,
-                       abs_mag_lim_lo=abs_mag_lim_lo, abs_mag_lim_hi=abs_mag_lim_hi,
-                       r_max=ananke_r_max)
     
-    # Run ananke
-    ananke.run()
-   
+    # Now we can prepare the ananke surveyor. Default surveyor is set to simulate a Roman + HST photometric system.
+    #fsample = 0.005
+    
+    surveyor = an.Ananke(p, name=ananke_name, fsample=fsample, rSun0=0, rSun1=0, rSun2=0, 
+                         photo_sys=photo_sys, cmd_magnames=cmd_magnames, abs_mag_lim_hi=abs_mag_lim_hi)
+    
+    survey = surveyor.run()
+  
