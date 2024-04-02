91fca8d9e6be993490b311b71ba13a7606518344
Default user <aeden2019@my.fit.edu>
2024-04-02 16:32:01 +0000

    Added functions for conversions and plotting.

---
+++ src/analysis/getConversions.py
@@ -0,0 +1,61 @@
+"""
+Functions that return commonly used conversions. 
+
+
+"""
+
+import numpy as np
+
+
+
+def abs2app(M, d, A=0.0):
+    """
+    Converts absolute magnitude to apparent magnitude.
+    
+    Parameters:
+    -----------
+    M : float
+        Absolute magnitude (mag).
+    d : float
+        Distance (pc). 
+    A : float
+        Extinction (mag/pc).
+    
+    
+    Returns:
+    --------
+    m : float
+        Apparent magnitude (mag).  
+    """
+    
+    m = 5 * np.log10(d) - 5 + M + A*d
+    
+    return m
+
+
+
+def app2abs(m, d, A=0.0):
+    """
+    Converts apparent magnitude to absolute magnitude.
+    
+    Parameters:
+    -----------
+    m : float
+        Apparent magnitude (mag).
+    d : float
+        Distance (pc). 
+    A : float
+        Extinction (mag/pc).
+    
+    
+    Returns:
+    --------
+    M : float
+        Absolute magnitude (mag).  
+    """
+    
+    M = m - 5 * np.log10(d) + 5 - A*d
+    
+    return M
+
+
---
+++ src/analysis/getPlots.py
@@ -0,0 +1,42 @@
+"""
+Functions for creating regularly used plots, such as color magnitude diagrams. 
+
+"""
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+
+
+def colormagplot(xaxis, yaxis):
+    """
+    Create color magnitude diagram
+    
+    input:
+    xaxis = array with g-r data
+    yaxis = array with M_g data
+    
+    """
+    
+    # Define hex cmap
+    hex_cmap = 'cividis'
+    hex_cmap = plt.get_cmap(hex_cmap)
+
+    # Create the subplot
+    fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(14, 6))
+
+    # Create the hexbin, with 241 log bins and the hex_cmap colormap
+    hb = axs.hexbin(xaxis, yaxis, gridsize=(241), bins='log', cmap=hex_cmap)
+
+    # Setting labels
+    axs.set_xlabel('$g - r$')
+    axs.set_ylabel('$M_g$')
+    axs.set_aspect(1.0/axs.get_data_ratio())  # Set aspect ratio
+    axs.invert_yaxis()
+
+    # Colorbar
+    cbar = fig.colorbar(hb, ax=axs)
+    cbar.set_label('Number of stars per hexcell')
+
+    plt.tight_layout()
+    plt.show()
