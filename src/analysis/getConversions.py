3f2abcee18b09e11799e9e2b4cf8cfc8da558427
Default user <aeden2019@my.fit.edu>
2024-04-25 19:24:34 +0000

    Galactic transformation and population plotting

--- src/analysis/getConversions.py
+++ src/analysis/getConversions.py
@@ -3,7 +3,8 @@ Functions that return commonly used conversions.
 
 
 """
-
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 import numpy as np
 
 
@@ -59,6 +60,39 @@ def app2abs(m, d, A=0.0):
     M = m - 5 * np.log10(d) + 5 - A*d
     
     return M
+
+
+
+
+def xyz_to_galactic(x, y, z):
+    """
+    Converts cartesian coordinates to galactic coordinates in a galactocentric model. 
+    
+    Parameters:
+    -----------
+    x : float
+        x position (kpc).
+    y : float
+        y position (kpc).
+    z : float
+        z position (kpc).
+    
+    
+    Returns:
+    --------
+    l : float
+        Galactic longitude (degrees).
+    b : float 
+        Galactic latitude (degrees).
+    """
+    
+    # Create SkyCoord object from XYZ coordinates
+    xyz = SkyCoord(x=x*u.kpc, y=y*u.kpc, z=z*u.kpc, frame='galactocentric', representation_type='cartesian')
+    
+    # Transform XYZ to Galactic coordinates (l, b, distance)
+    galactic = xyz.transform_to('galactic')
+    
+    return galactic.l.deg, galactic.b.deg
 
 
 
